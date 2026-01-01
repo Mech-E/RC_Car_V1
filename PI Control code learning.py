9b33557c0ecc41382628d713b19ab6a5b94408ad
Andrew Schaffer <mech.e.andrewschaffer@gmail.com>
2026-01-01 15:11:06 -0600

    Update

--- PI Control code learning.py
+++ PI Control code learning.py
@@ -3,14 +3,12 @@ import time
 import subprocess
 
 # Starts the mpremote run command for the file on ESP32
-subprocess.run(['mpremote', 'connect','auto', 'run', 'PI Control code learning.py'], check=True)
+subprocess.run(['mpremote', 'connect','auto', 'run', 'MicroPython Servo Learning.py'], check=True)
 time.sleep(0.5)
 
 ser = serial.Serial('/dev/ttyUSB0', 115200)
 time.sleep(2)  # allow Pico to reboot
 
-def send_angle(angle):
-    msg = f"Set {angle}\n"
 def send_angle(angle):
     msg = f"SET {angle}\n"
     print(f"[PI] Sending: {msg.strip()}")
