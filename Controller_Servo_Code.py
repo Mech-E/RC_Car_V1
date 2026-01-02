cec23b75380ab2119e4bbbd92baef4ccda0faa01
Andrew Schaffer <mech.e.andrewschaffer@gmail.com>
2026-01-02 16:47:00 -0600

    Controller Update

--- Controller_Servo_Code.py
+++ Controller_Servo_Code.py
@@ -1,9 +1,24 @@
 from evdev import InputDevice, categorize, ecodes
-
+import time
+import subprocess
+import serial
 # Find the controller path: ls /dev/input/
 
 gamepad = InputDevice('/dev/input/event4') 
 print('Listening for controller events.....')
+
+# Start ESP32 script without blocking
+proc = subprocess.Popen(['mpremote', 'connect', 'auto', 'run', 'MicroPython_Servo_Learning.py'])
+
+time.sleep(1)  # give ESP32 time to boot and start listening
+
+ser = serial.Serial('/dev/ttyUSB0', 115200)
+time.sleep(2)
+
+def send_angle(angle):
+    msg = f"SET {angle}\n"
+    print(f"[PI] Sending: {msg.strip()}")
+    ser.write(msg.encode())
 
 for event in gamepad.read_loop():
     if event.type == ecodes.EV_KEY:
@@ -14,5 +29,8 @@ for event in gamepad.read_loop():
         absevent = categorize(event)
         print(f"Axis: {absevent.event.code}, Value: {absevent.event.value}")
 
+    if absevent == '2':
+        angle = (absevent.event.value/1023)*180
+        send_angle(angle)
 
-              
+        
