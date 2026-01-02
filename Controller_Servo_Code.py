6e4d4d79a23c2e27cb78e9fb7d44b52310fcd4cb
Andrew Schaffer <mech.e.andrewschaffer@gmail.com>
2026-01-02 11:46:45 -0600

    Controller

---
+++ Controller_Servo_Code.py
@@ -0,0 +1,18 @@
+from edev import Inputdevice, categorize, ecodes
+
+# Find the controller path: ls /dev/input/
+
+gamepad = InputDevice('/dev/input/event0') 
+print('Listening for controller events.....')
+
+for event in gamepad.read_loop():
+    if event.type == ecodes.EV_KEY:
+        keyevent = categorize(event)
+        print(f"Button: {keyevent.keycode}, State: {keyevent.keystate}")
+
+    if event.type == ecodes.EV_ABS:
+        absevent = categorize(event)
+        print(f"Axis: {absevent.event.code}, Value: {absevent.event.value}")
+
+
+              
