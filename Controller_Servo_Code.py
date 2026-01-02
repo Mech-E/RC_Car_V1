1b33ed47de637fc1348338ca45f2e33cf0885cd3
Andrew Schaffer <mech.e.andrewschaffer@gmail.com>
2026-01-02 13:36:15 -0600

    Spelling again

--- Controller_Servo_Code.py
+++ Controller_Servo_Code.py
@@ -2,7 +2,7 @@ from evdev import InputDevice, categorize, ecodes
 
 # Find the controller path: ls /dev/input/
 
-gamepad = InputDevice('/dev/input/event0') 
+gamepad = InputDevice('/dev/input/event4') 
 print('Listening for controller events.....')
 
 for event in gamepad.read_loop():
