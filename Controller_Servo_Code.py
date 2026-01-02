c87678a2387c6fbfece29b1333abc50b11472165
Andrew Schaffer <mech.e.andrewschaffer@gmail.com>
2026-01-02 17:35:53 -0600

    Update

--- Controller_Servo_Code.py
+++ Controller_Servo_Code.py
@@ -1,13 +1,11 @@
 from evdev import InputDevice, categorize, ecodes
 import time
-import subprocess
 import serial
-# Find the controller path: ls /dev/input/
 
-gamepad = InputDevice('/dev/input/event4') 
+gamepad = InputDevice('/dev/input/event4')
 print('Listening for controller events.....')
 
-time.sleep(1)  # give ESP32 time to boot and start listening
+time.sleep(1)
 
 ser = serial.Serial('/dev/ttyUSB0', 115200)
 time.sleep(2)
@@ -17,17 +15,20 @@ def send_angle(angle):
     print(f"[PI] Sending: {msg.strip()}")
     ser.write(msg.encode())
 
+def trigger_con(raw_value):
+    angle = (raw_value / 1023) * 180
+    return angle
+
 for event in gamepad.read_loop():
-    if event.type == ecodes.EV_KEY:
-        keyevent = categorize(event)
-        print(f"Button: {keyevent.keycode}, State: {keyevent.keystate}")
 
     if event.type == ecodes.EV_ABS:
         absevent = categorize(event)
-        print(f"Axis: {absevent.event.code}, Value: {absevent.event.value}")
+        code = absevent.event.code
+        value = absevent.event.value
 
-    if absevent.event.code == '2':
-        angle = (absevent.event.value/1023)*180
-        send_angle(angle)
+        print(f"Axis: {code}, Value: {value}")
 
-        
+        # Check for ABS_Z (axis 2)
+        if code == ecodes.ABS_Z:
+            angle = trigger_con(value)
+            send_angle(angle)
