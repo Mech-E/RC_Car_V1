d6c075a1296431da89c827f238c73851ffbf304d
Andrew Schaffer <mech.e.andrewschaffer@gmail.com>
2026-01-03 18:08:08 -0600

    ergrer

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ time.sleep(2)
 def convert(raw):
     return int((raw / 1023) * 180)
 
-# Servo Angle Function
 def send_angle(angle):
     msg = f"SET {angle}\n"
     print(f"[PI] Sending: {msg.strip()}")
@@ -21,24 +20,25 @@ def handle_input(input_name, value):
     global current_trig_ang, locked_ang
     print("INPUT:", input_name, value)
 
+    # TRIGGER MOVEMENT — update angle but DO NOT send
     if input_name == "Left Trigger":
         current_trig_ang = convert(value)
-        send_angle(current_trig_ang)
         print(f"Trigger angle = {current_trig_ang}")
         return
 
+    # BUTTON A — lock angle
     if input_name == "Button A" and value == 1:
         locked_ang = current_trig_ang
-        print(f"[LCOK] Servo set to {locked_ang}")
+        print(f"[LOCK] Servo set to {locked_ang}")
         send_angle(locked_ang)
         return
-    
+
+    # BUTTON B — reset
     if input_name == "Button B" and value == 1:
         locked_ang = None
         print("[RESET] Servo set to 0")
         send_angle(0)
         return
-    
 
 listener = ControllerListener('/dev/input/event4', handle_input)
 listener.start()
