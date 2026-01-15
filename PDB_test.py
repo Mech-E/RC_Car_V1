fe8926be192e03586b6bbbfbfd11daab2dd847bb
Andrew Schaffer <mech.e.andrewschaffer@gmail.com>
2026-01-15 16:37:41 -0600

    PC Update

--- PDB_test.py
+++ PDB_test.py
@@ -1,20 +1,43 @@
-from machine import Pin, I2C
-from pca9685 import PCA9685
+# -----------------------------
+# PCA9685 DRIVER (embedded)
+# -----------------------------
+from machine import I2C
 import time
 
-# Initialize I2C0 on GP0 (SDA) and GP1 (SCL)
+class PCA9685:
+    def __init__(self, i2c, address=0x40):
+        self.i2c = i2c
+        self.address = address
+        self.reset()
+
+    def reset(self):
+        self.write8(0x00, 0x00)
+
+    def write8(self, reg, value):
+        self.i2c.writeto_mem(self.address, reg, bytes([value]))
+
+    def set_pwm_freq(self, freq):
+        prescale = int(25000000.0 / (4096 * freq) - 1)
+        oldmode = self.i2c.readfrom_mem(self.address, 0x00, 1)[0]
+        newmode = (oldmode & 0x7F) | 0x10
+        self.write8(0x00, newmode)
+        self.write8(0xFE, prescale)
+        self.write8(0x00, oldmode)
+        time.sleep_ms(5)
+        self.write8(0x00, oldmode | 0xa1)
+
+    def set_pwm(self, channel, on, off):
+        data = bytearray([on & 0xFF, on >> 8, off & 0xFF, off >> 8])
+        self.i2c.writeto_mem(self.address, 0x06 + 4 * channel, data)
+
+# -----------------------------
+# YOUR NORMAL CODE
+# -----------------------------
+from machine import Pin, I2C
+
 i2c = I2C(0, sda=Pin(0), scl=Pin(1))
-
-# Initialize PCA9685
 pca = PCA9685(i2c)
-pca.freq(50)  # Standard servo frequency
-
-SERVO_CH = 1  # Servo plugged into PCA9685 channel 1
-
-# Typical servo pulse range (adjust for your servo)
-MIN = 150  # ~0°
-MID = 375  # ~90°
-MAX = 600  # ~180°
+pca.set_pwm_freq(50)
 
 # Move servo on channel 1
-pca.duty(SERVO_CH, MID)
+pca.set_pwm(1, 0, 375)
