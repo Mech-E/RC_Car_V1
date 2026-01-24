a4961c0b250a3a236465d2cc57ab0b5200ad490c
Andrew Schaffer <mech.e.andrewschaffer@gmail.com>
2026-01-24 09:35:14 -0600

    Update Constraints

--- Camera_Display.py
+++ Camera_Display.py
@@ -1,27 +1,23 @@
-import sys
 from picamera2 import Picamera2
-from picamera2.previews import FullscreenPreview
+from picamera2.previews import DrmPreview
+import sys
 
 
 class CameraDisplayGL:
     def __init__(self):
-        # Init camera
         self.picam2 = Picamera2()
 
-        # Choose a reasonable preview configuration
         config = self.picam2.create_preview_configuration()
         self.picam2.configure(config)
 
-        # Create OpenGL preview (fullscreen by default on Pi)
-        self.preview = OpenGLPreview()
+        # Use the preview backend your system actually supports
+        self.preview = DrmPreview()
 
     def run(self):
-        # Start camera with OpenGL preview
         self.picam2.start(show_preview=self.preview)
 
         try:
-            # Block forever; preview runs in its own loop
-            print("Camera running with OpenGL preview. Press Ctrl+C to quit.")
+            print("Camera running. Press Ctrl+C to quit.")
             while True:
                 pass
         except KeyboardInterrupt:
